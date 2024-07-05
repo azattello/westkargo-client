e1f928fa16cc1c9f1c89b48e707795e9e041313e
Abdykali Azat <azat.abdykalli@gmail.com>
2024-07-06 03:39:54 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://westkargo.kz:3001',
+    
+    nameCargo: 'West Kargo'
+  };
+  
+  
+export default config;
+  
